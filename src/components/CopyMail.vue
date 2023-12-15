20b69bdbc58a2341b120f12f3fb64896f9799509
Sebastian <sebastian.contreras@praxedes.co>
2023-12-14 19:08:07 -0500

    Portfolio V2 Green Style

---
+++ src/components/CopyMail.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="relative group/tooltip">
+        <button @click="msgCopy = true; copyContent()"
+            class="flex bg-emerald-700 hover:bg-opacity-70 py-2.5 px-4 rounded-lg">
+            <slot></slot>
+        </button>
+        <span
+            class="absolute pointer-events-none transition-all opacity-0 z-20 bottom-full -translate-y-0 py-1 px-1.5 text-xs left-1/2 -translate-x-1/2 rounded-md whitespace-nowrap text-gray-200 bg-gray-800 before:content-[''] before:absolute before:bg-gray-800 before:rounded-sm before:w-2.5 before:rotate-45 before:h-2.5 before:-bottom-1 before:-z-10 before:left-1/2 before:-translate-x-1/2 group-hover/tooltip:opacity-100 group-hover/tooltip:-translate-y-3">{{
+                msgCopy ? 'Copied' : 'Copy email' }}</span>
+    </div>
+</template>
+  
+<script setup lang="ts">
+import { ref } from 'vue'
+var msgCopy = ref(false);
+
+const copyContent = async () => {
+    try {
+        await navigator.clipboard.writeText('sebas.contreras.ceballos@gmail.com');
+    } catch (err) {
+        console.error('Failed to copy: ', err);
+    }
+}
+</script>
--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -2,31 +2,26 @@ export function trimText(input: string, maxLength: number = 100): string {
   if (input.length <= maxLength) return input;
   return input.substring(0, maxLength - 3) + "...";
 }
-export function getCurrentTimeInItaly(): Date {
-  // Create a date object with the current UTC time
+export function getCurrentTimeInColombia(): Date {
   const now = new Date();
 
-  // Convert the UTC time to Italy's time
-  const offsetItaly = 2; // Italy is in Central European Summer Time (UTC+2), but you might need to adjust this based on Daylight Saving Time
-  now.setHours(now.getUTCHours() + offsetItaly);
+  const offsetColombia = -5;
+  now.setHours(now.getUTCHours() + offsetColombia);
 
   return now;
 }
 
-export function formatTimeForItaly(date: Date): string {
+export function formatTimeForColombia(date: Date): string {
   const options: Intl.DateTimeFormatOptions = {
     hour: "numeric",
     minute: "2-digit",
     second: "2-digit",
-    hour12: true, // This will format the time in 12-hour format with AM/PM
-    timeZone: "Europe/Rome",
+    hour12: true,
+    timeZone: "America/Bogota",
   };
 
   let formattedTime = new Intl.DateTimeFormat("en-US", options).format(date);
-
-  // Append the time zone abbreviation. You can automate this with libraries like `moment-timezone`.
-  // For simplicity, here I'm just appending "CET", but do remember that Italy switches between CET and CEST.
-  formattedTime += " CET";
+  formattedTime += " COT";
 
   return formattedTime;
 }
@@ -37,4 +32,17 @@ export function formatDate(date: Date): string {
     month: "long",
     day: "numeric",
   });
+}
+
+export function formatWeekDay(date: Date): string {
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return weekday[date.getDay()];
 }
